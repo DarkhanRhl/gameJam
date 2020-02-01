969f03da6e275229ef3985905cda69ff1e7ac3b6
maxime <maxime.aiguier@epitech.eu>
2020-02-01 14:10:40 +0100

    add first window

--- src/main.py
+++ src/main.py
@@ -1,34 +1,24 @@
-# import the pygame module, so you can use it
 import pygame
+from pygame.locals import *
+
  
-# define a main function
 def main():
      
-    # initialize the pygame module
     pygame.init()
-    # load and set the logo
-    logo = pygame.image.load("assets/raphael.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
      
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
+    window = pygame.display.set_mode((920,680))
      
-    # define a variable to control the main loop
+    background = pygame.image.load("assets/weiqi-cover.jpg").convert()
+    window.blit(background, (0,0))
+    pygame.display.flip()
+
     running = True
      
-    # main loop
     while running:
-        # event handling, gets all event from the event queue
         for event in pygame.event.get():
-            # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
                 running = False
      
-     
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
+
 if __name__=="__main__":
-    # call the main function
     main()
