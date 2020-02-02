d24524b7eec1b93ac6eb6520bfead514a1761af4
maxime <maxime.aiguier@epitech.eu>
2020-02-02 13:29:56 +0100

    feat(pieces) : add drop pieces

--- src/core.py
+++ src/core.py
@@ -1,6 +1,9 @@
 import pygame
 from player import Player
 from wall import Wall
+from pieces import Pieces
+
+pygame.init()
 
 class Core:
     FPS = 60
@@ -21,8 +24,20 @@ class Core:
         "right": pygame.K_d
     }
 
+    # index des positions : 
+    # 0 = square
+    # 1 = l droit
+    # 2 = l gauche
+    # 3 = t
+    # 4 = barre vertical
+    # 5 = barre horizontal
+
     POS_WALL_1 = [[0,30], [0, 150], [0, 270], [0, 390], [0, 510], [0, 630]]
     POS_WALL_2 = [[1048 ,30], [1048, 150], [1048, 270], [1048, 390], [1048, 510], [1048, 630]]
+
+    #middle : 540|360
+    POS_PIECES = [[540, 310], [570, 330], [540, 360], [610, 360], [600, 360], [570, 390],
+                  [520, 310], [490, 330], [500, 360], [440, 360], [460, 360], [490, 390]]
 
     def __init__(self):
         pygame.init()
@@ -35,6 +50,8 @@ class Core:
         self.objects.append(Player(self.window, self.PLAYER2_KEYS))
         self.objects.append(Wall(self.window, self.POS_WALL_1))
         self.objects.append(Wall(self.window, self.POS_WALL_2))
+        self.objects.append(Pieces(self.window, self.POS_PIECES))
+
 
 
     def eventManager(self):
